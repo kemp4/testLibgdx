df3986998552a90ec79700a61058e53a2e87b268
Szymon Kempa <kempa44@gmail.com>
2017-09-25 07:23:22 +0200

    initial commit

---
+++ core/src/pl/skempa/Node.java
@@ -0,0 +1,35 @@
+package pl.skempa;
+
+public class Node {
+	protected long id;
+	protected boolean visible;
+	protected float lat;
+	protected float lon;
+
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public boolean isVisible() {
+		return visible;
+	}
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getLon() {
+		return lon;
+	}
+	public void setLon(float lon) {
+		this.lon = lon;
+	}
+
+}
---
+++ desktop/src/pl/skempa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.skempa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.skempa.LibgdxTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibgdxTest(), config);
+	}
+}
