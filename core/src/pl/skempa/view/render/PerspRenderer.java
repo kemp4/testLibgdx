ebac11f83a3c08c2436c5926fb0822f4c891f7e7
Szymon Kempa <szymkem864@student.polsl.pl>
2017-12-08 12:30:27 +0100

    orthoRendererWithShader generating way mesh fix
    Former-commit-id: 098eb3edec2f6ffba0786c3b367bcd41c0ff2eb6

--- core/src/pl/skempa/view/render/PerspRenderer.java
+++ core/src/pl/skempa/view/render/PerspRenderer.java
@@ -1,36 +1,8 @@
 package pl.skempa.view.render;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-
-import pl.skempa.model.Model;
-import pl.skempa.shader.ShaderWrapper;
-
 /**
- * Created by Mymon on 2017-10-08.
+ * Created by szymk on 12/8/2017.
  */
 
 public class PerspRenderer implements ObjectsRenderer {
-    private Camera camera;
-    private ShaderWrapper shaderWrapper;
-
-
-
-    @Override
-    public void renderObjects(Model model) {
-
-        ShaderProgram shader = shaderWrapper.getShaderProgram();
-        this.camera.update();
-        //texture.bind();
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        shader.begin();
-        shader.setUniformMatrix("u_projTrans", this.camera.combined);
-        shader.setUniformi("u_texture", 0);
-      //  mesh.render(shader, GL20.GL_TRIANGLES);
-        shader.end();
-    }
-
-
 }
